c276ee74b7221b56f72af0de304fdaed5987c3d2
Jeniffer Carvalho <jeniffercarvalho2025@gmail.com>
2026-05-29 01:07:01 -0300

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -87,7 +87,7 @@ export default function App() {
 
             {/* IMAGE */}
             <img
-              src="public/careermatch.png"
+              src="careermatch.png"
               alt="character"
               className="heroImage"
             />
